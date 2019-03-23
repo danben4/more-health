c865f705f35bd3f4222b8a92a8673549bf21a6cc
danben4 <danielbenediktsson4@gmail.com>
2019-03-23 19:55:44 +0000

    header

--- src/components/Header.js
+++ src/components/Header.js
@@ -1,8 +1,17 @@
 import React from 'react';
 
+import './Header.css';
+
 const Header = () => {
   return (
-    <div>Header!</div>
+    <div className="header">
+      <div className="innerHeader">
+        <div>More health</div>
+        <div>
+          TODO
+        </div>
+      </div>
+    </div>
   )
 };
 
