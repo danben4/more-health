d755332daa05c7e9ebbb847d294e567940e9b043
danben4 <danielbenediktsson4@gmail.com>
2019-03-23 13:25:03 +0000

    setup auth consumer for firebase

--- src/App.js
+++ src/App.js
@@ -1,13 +1,30 @@
 import React, { Component } from 'react';
-import logo from './logo.svg';
+import * as firebase from "firebase/app";
+import "firebase/auth";
+import {
+  FirebaseAuthProvider,
+  FirebaseAuthConsumer
+} from "@react-firebase/auth";
+import { config } from "./firebaseConfig";
 import './App.css';
 
 class App extends Component {
   render() {
     return (
-      <div className="root">
-        MORE HEALTH!
-      </div>
+      <FirebaseAuthProvider {...config} firebase={firebase}>
+        <div className="root">
+          <div>Inside FirebaseAuthProvider</div>
+          <FirebaseAuthConsumer>
+            {({ isSignedIn }) => {
+              if (isSignedIn === true) {
+                return "Signed in";
+              } else {
+                return "Not signed in";
+              }
+            }}
+          </FirebaseAuthConsumer>
+        </div>
+      </FirebaseAuthProvider>
     );
   }
 }
