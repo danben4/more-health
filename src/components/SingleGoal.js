73f78965b07c4eb9384a99061f4d86cb75964c37
danben4 <danielbenediktsson4@gmail.com>
2019-03-23 23:10:17 +0000

    active goal mutation

--- src/components/SingleGoal.js
+++ src/components/SingleGoal.js
@@ -1,19 +1,39 @@
 import React from 'react';
+import { FirebaseDatabaseMutation } from "@react-firebase/database";
 
 import Button from './Button';
 import './SingleGoal.css';
 
-const SingleGoal = ({goal}) => {
-  console.log("single goal", goal);
+const SingleGoal = ({goalId, goal, userId}) => {
+  const seconds = 3600 * 24 * goal.duration;
+  const today = Math.round((new Date()).getTime() / 1000);
+  const endDate = today + seconds;
   return (
     <>
       <div className="singleGoal">
         <div className="singleGoalDescription">{goal.description}</div>
         <img alt="" src={goal.imageUrl} className="singleGoalImage" /> :
       </div>
-      <Button onClick={() => console.log("CLICK!!")}>
-        Activate goal!
-      </Button>
+      <FirebaseDatabaseMutation type="push" path={"users/" + userId + "/usergoals/" + goalId}>
+        {({ runMutation }) => (
+          <Button
+            onClick={() => {
+              runMutation({
+                companyName: goal.companyName,
+                description: goal.description,
+                endDate: endDate,
+                imageUrl: goal.imageUrl,
+                isComplete: 0,
+                name: goal.name,
+                startDate: today,
+                totalCompleted: 0.0
+              })
+            }}
+          >
+            Activate goal!
+          </Button>
+        )}
+      </FirebaseDatabaseMutation>;
     </>
   )
 };
