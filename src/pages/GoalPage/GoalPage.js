139c3c55b7013f38e8b0b3da5a75c38f417d548a
danben4 <danielbenediktsson4@gmail.com>
2019-03-23 22:00:24 +0000

    remove unused stuff

--- src/pages/GoalPage/GoalPage.js
+++ src/pages/GoalPage/GoalPage.js
@@ -2,8 +2,6 @@ import React, { Component } from 'react';
 
 class GoalPage extends Component {
   render() {
-    console.log("this.props.", this.props);
-  //  let params = new URLSearchParams(this.props.location.search);
     return (
       <div>
         GoalPage!
