12ef3b48857f1a2dd939c96575c44ed2b4a8e1a6
Fannar Magnússon <fannar@kth.se>
2019-03-23 17:08:57 +0000

    rename import digime to sync digime data

--- api/digime/main.py
+++ api/digime/main.py
@@ -1,12 +1,12 @@
 import flask
 
-def __import_digime_data(request):
-    return 'Here digi.me data should be imported.'
+def __sync_digime_data(request):
+    return 'Here we can sync digi.me data'
 
 def digime_data(request):
     if request.path == '/' or request.path == '':
         if request.method == 'POST':
-            return __import_digime_data(request)
+            return __sync_digime_data(request)
         else:
             return 'Method not supported', 405
     return 'URL not found', 404
