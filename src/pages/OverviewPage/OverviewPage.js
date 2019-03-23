f5843affa9c467dc1bedb0365c71cbabf7f2f284
danben4 <danielbenediktsson4@gmail.com>
2019-03-23 21:00:58 +0000

    filter functionality

--- src/pages/OverviewPage/OverviewPage.js
+++ src/pages/OverviewPage/OverviewPage.js
@@ -7,27 +7,14 @@ import EmptyResponse from '../../components/EmptyResponse';
 import Goals from '../../components/Goals';
 import Seperator from '../../components/Seperator';
 
-const filterActiveGoals = (goals) => Object.keys(goals).map(
-  key => {
-    return {
-      key,
-      ...goals[key]
-    };
-  }
-);
+const filterActiveGoals = (goals) =>
+  Object.keys(goals).map(
+    key => {return {key, ...goals[key]}}
+  ).filter(goal => new Date() < new Date(goal.endDate));
 
-// return Object.keys(goals).map(
-//   key =>
-//     <Goal key={key} goal={goals[key]} isCompanyGoal={false} />
-// );
+const filterCompletedGoals = (activeGoals) => activeGoals.filter(goal => goal.totalCompleted >= 1);
 
-/*
-
-const filterCompletedGoals = (goals) => "TODO";
-
-const filterInCompletedGoals = (goals) => "TODO";
-
-*/
+const filterInCompletedGoals = (activeGoals) => activeGoals.filter(goal => goal.totalCompleted < 1);
 
 class OverviewPage extends Component {
   render() {
@@ -41,13 +28,22 @@ class OverviewPage extends Component {
               if (data.isLoading) return <Loading />;
               if (! data.value) return <EmptyResponse text="No goals!" />;
               const activeGoals = filterActiveGoals(data.value.usergoals);
+              let inCompletedGoals = filterInCompletedGoals(activeGoals);
+              let completedGoals = filterCompletedGoals(activeGoals);
               return (
                 <>
                   <Heading text="Active Goals" />
-                  <Goals goals={activeGoals} />
+                  {
+                    inCompletedGoals.length === 0 ?
+                    <EmptyResponse text="No active goals!" /> :
+                    <Goals goals={inCompletedGoals} />
+                  }
                   <Seperator />
                   <Heading text="Completed Goals" />
-                  <Goals goals={activeGoals} />
+                  {completedGoals.length === 0 ?
+                    <EmptyResponse text="No completed goals!" /> :
+                    <Goals goals={completedGoals} />
+                  }
                 </>
               );
             }}
