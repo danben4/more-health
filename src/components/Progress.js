9d3389c67edcb8a43cfd91f29cd953539c50d2f0
danben4 <danielbenediktsson4@gmail.com>
2019-03-23 19:17:42 +0000

    more styling

--- src/components/Progress.js
+++ src/components/Progress.js
@@ -3,9 +3,9 @@ import React from 'react';
 import './Progress.css';
 
 const getBarColor = (totalCompleted) => {
-  if (totalCompleted > 0.8) return "green";
-  if (totalCompleted > 0.4) return "yellow";
-  return "red";
+  if (totalCompleted > 0.8) return "#7dbd32";
+  if (totalCompleted > 0.4) return "#efc030";
+  return "#ed344e";
 };
 
 const Progress = ({totalCompleted}) => {
@@ -18,7 +18,7 @@ const Progress = ({totalCompleted}) => {
           backgroundColor: getBarColor(totalCompleted)
         }}
       />
-      <span className="number">{totalCompleted * 100}%</span>
+      <span className="number">{totalCompleted * 100}% completed!</span>
     </div>
   )
 };
