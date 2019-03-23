e222ee25e985a88b615a0a56ff6016c7c6b6d4a8
Sindri Már Kaldal Sigurjónsson <sindrimars@gmail.com>
2019-03-23 13:53:27 +0000

    api for coupons

---
+++ api/coupons/main.py
@@ -0,0 +1,76 @@
+import firebase_admin
+from firebase_admin import db
+import flask
+# import os
+# os.environ["GOOGLE_APPLICATION_CREDENTIALS"] = "/Users/eysteinngunnlaugsson/Documents/healthcare_hackathon/actual_project/api/more-health-firebase-adminsdk-z3um3-7eecbde28b.json"
+
+
+firebase_admin.initialize_app(options={
+    'databaseURL': 'https://more-health.firebaseio.com/',
+})
+
+COUPONS = db.reference('coupons')
+
+
+def __get_coupon_by_id(id):
+    return COUPONS.child(id).get()
+
+
+def create_coupon(request):
+    req = request.json
+    user = COUPONS.push(req)
+    return flask.jsonify({'id': user.key}), 201
+
+
+def get_all_coupons():
+    coupons = COUPONS.get()
+    if not coupons:
+        return 'No coupons found', 404
+    coupons = [coupon for coupons in coupons.values()]
+    return flask.jsonify(coupons)
+
+
+def get_coupon(id):
+    coupon = __get_coupon_by_id(id)
+    if not coupon:
+        return 'Coupon not found', 404
+    return flask.jsonify(coupon)
+
+
+def delete_coupon(id):
+    uscouponer = __get_coupon_by_id(id)
+    if not coupon:
+        return 'Coupon not found', 404
+    COUPONS.child(id).delete()
+    return flask.jsonify({'success': True})
+
+
+def update_coupon(id, request):
+    user = __get_coupon_by_id(id)
+    if not coupon:
+        return 'Coupon not found', 404
+    req = request.json
+    COUPONS.child(id).update(req)
+    return flask.jsonify({'success': True})
+
+
+
+def coupons(request):
+    if request.path == '/' or request.path == '':
+        if request.method == 'POST':
+            return create_coupon(request)
+        elif request.method == 'GET':
+            return get_all_coupons()
+        else:
+            return 'Method not supported', 405
+    if request.path.startswith('/'):
+        id = request.path.lstrip('/')
+        if request.method == 'GET':
+            return get_coupon(id)
+        elif request.method == 'DELETE':
+            return delete_coupon(id)
+        elif request.method == 'PUT':
+            return update_coupon(id, request)
+        else:
+            return 'Method not supported', 405
+    return 'URL not found', 404
