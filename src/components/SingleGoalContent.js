33d5d5d086f1019e30834ad916d72a77d9761319
danben4 <danielbenediktsson4@gmail.com>
2019-03-24 02:48:17 +0000

    flot

--- src/components/SingleGoalContent.js
+++ src/components/SingleGoalContent.js
@@ -2,12 +2,16 @@ import React from 'react';
 import { FirebaseDatabaseMutation } from "@react-firebase/database";
 
 import Progress from './Progress';
-import SingleGoalButtons from './SingleGoalButtons';
+import Button from './Button';
+import ShareButton from './ShareButton';
 import './SingleGoalContent.css';
 
 class SingleGoalContent extends React.Component {
   render() {
     const { data, goal, userId, goalId } = this.props;
+    const seconds = 3600 * 24 * goal.duration;
+    const today = Math.round((new Date()).getTime() / 1000);
+    const endDate = today + seconds;
     return (
       <div className="singleGoal">
         <img alt="" src={goal.imageUrl} className="singleGoalImage" />
@@ -27,15 +31,35 @@ class SingleGoalContent extends React.Component {
                   >
                     {({runMutation}) => {
                       return (
-                        <SingleGoalButtons
-                          runMutation={runMutation}
-                          goal={goal}
-                        />
+                        <div className="singleGoalButtons">
+                          <ShareButton />
+                          <Button
+                            onClick={() => {
+                              runMutation({
+                                companyName: goal.companyName,
+                                description: goal.description,
+                                endDate: endDate,
+                                imageUrl: goal.imageUrl,
+                                isComplete: 0,
+                                name: goal.name,
+                                startDate: today,
+                                totalCompleted: 0.0,
+                                category: goal.category,
+                                total: goal.metric,
+                              })
+                            }}
+                          >
+                            Activate goal!
+                          </Button>
+                        </div>
                       );
                     }}
                   </FirebaseDatabaseMutation> :
                   <div className="progressContent">
-                    <Progress totalCompleted={data.value.totalCompleted} />
+                    <ShareButton />
+                    <div className="progressBar">
+                      <Progress totalCompleted={data.value.totalCompleted} />
+                    </div>
                   </div>
               }
             </div>
