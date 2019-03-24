abcbc4fa29ea2c9044ef9b77d07ee54b68bc8719
Sindri Már Kaldal Sigurjónsson <sindrimars@gmail.com>
2019-03-24 01:11:55 +0000

    modified brand name in header

--- src/components/Header.js
+++ src/components/Header.js
@@ -7,7 +7,7 @@ const Header = () => {
   return (
     <div className="header">
       <div className="innerHeader">
-        <Link to="/overview/" className="headerBrandName">More health</Link>
+        <Link to="/overview/" className="headerBrandName">M O R E</Link>
         <div className="headerLinks">
           <Link to="/activities/" className="headerLink">Activities</Link>
           <Link to="/goals/" className="headerLink">All goals</Link>
